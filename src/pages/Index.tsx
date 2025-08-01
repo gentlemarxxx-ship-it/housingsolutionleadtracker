76f01e36abeb1b123d7981e018b4402c29b6823e
Mark Anthony Jalandoni <markyjalandoni.mj@gmail.com>
2025-08-01 23:49:18 +0800

    [dyad] Filter main page to show only 'Leads' - wrote 1 file(s) + extra files edited outside of Dyad

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,8 +3,9 @@ import { LeadsPage } from "./LeadsPage"
 const Index = () => {
   return (
     <LeadsPage
-      title="All Leads"
-      description="Manage all your leads in one place. Filter, edit, and track the status of each lead."
+      remarkFilter="Leads"
+      title="Leads"
+      description="Manage all active leads that are currently in the pipeline."
     />
   );
 };
